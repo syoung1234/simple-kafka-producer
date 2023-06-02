27b08c912bf9f4c29709ef4a1b8184a833012a24
syoung1234 <syoung8818@gmail.com>
2023-06-02 21:38:16 +0900

    feat: add custom partitioner

---
+++ src/main/java/simple/CustomPartitioner.java
@@ -0,0 +1,37 @@
+package simple;
+
+import org.apache.kafka.clients.producer.Partitioner;
+import org.apache.kafka.common.Cluster;
+import org.apache.kafka.common.InvalidRecordException;
+import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.utils.Utils;
+
+import java.util.List;
+import java.util.Map;
+
+public class CustomPartitioner implements Partitioner {
+
+    @Override
+    public int partition(String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes, Cluster cluster) {
+        if (keyBytes == null) {
+            throw new InvalidRecordException("Need message key");
+        }
+        if (((String)key).equals("Seoul")) {
+            return 1; // key가 Seoul 이라면 1번 파티션으로 전송
+        }
+
+        List<PartitionInfo> partitions = cluster.partitionsForTopic(topic);
+        int numPartitions = partitions.size();
+        return Utils.toPositive(Utils.murmur2(keyBytes)) % numPartitions;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void configure(Map<String, ?> configs) {
+
+    }
+}
